50a0cd591644522178b17e49100e2a5697ef4e9e
y-tec09 <112913753+y-tec09@users.noreply.github.com>
2023-10-16 13:16:37 -0600

    Subida principal de archivos de plantilla modelo 7
    Se agregan los archivos de la primera version de la plantilla modelo 7, es propenso a tener modificaciones en fechas posteriores.

---
+++ l/js/mostr_ocult.js
@@ -0,0 +1,21 @@
+//Mostrar
+function mostSec0() {
+  document.getElementById("secDet0").style.display = "block";
+}
+function mostSec1() {
+  document.getElementById("secDet1").style.display = "block";
+}
+function mostSec2() {
+  document.getElementById("secDet2").style.display = "block";
+}
+
+//Ocultar
+function ocultSec0() {
+  document.getElementById("secDet0").style.display = "none";
+}
+function ocultSec1() {
+  document.getElementById("secDet1").style.display = "none";
+}
+function ocultSec2() {
+  document.getElementById("secDet2").style.display = "none";
+}
